cd4770fb9878db9fc7f04e84b3eb26048cc28ec3
Arif <bozdag.arif@gmail.com>
2021-02-06 23:47:31 +0300

    ctrl holddown for multiple window jump support

--- main.py
+++ main.py
@@ -2,23 +2,34 @@
 
 import keyboard
 import time
-from datetime import datetime
 import traceback
 
-def main():
-    try:
-        print("hi")
-        keyboard.remap_hotkey('ctrl+tab', 'alt+tab')
-        while True:
-            time.sleep(0.5)
-    except:
-        traceback.print_exc()
+class alt_tab_modifier():
+    
+    def __init__(self): 
+        self.alt_tab = False
+        keyboard.on_press_key('tab', self.tab_event, suppress=True)
+        keyboard.on_release_key('ctrl', self.ctrl_release_event, suppress=True)
 
-def do_this():
-    try:
-        keyboard.press("a")
-    except:
-        pass
+    def tab_event(self, e):
+        if self.alt_tab:
+            pass            
+        elif keyboard.is_pressed('ctrl'):
+            keyboard.release('ctrl')
+            keyboard.press('alt')
+            self.alt_tab = True
+        keyboard.send('tab')
+
+    def ctrl_release_event(self, e):
+        if self.alt_tab:
+            keyboard.release('alt')
+            self.alt_tab = False
+        else:
+            keyboard.release('ctrl')
+
 
 if __name__ == "__main__":
-    main()
+    
+    modifier = alt_tab_modifier()
+    while True:
+        time.sleep(0.1)
